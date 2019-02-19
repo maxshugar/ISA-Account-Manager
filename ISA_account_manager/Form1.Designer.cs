0f0a39242ea1ee04fae32ab72b3ebed81d091899
Max Rochefort-Shugar <max.shugar@sony.com>
2019-02-19 22:46:04 +0000

    Working on function to add form controls to page dynamically. Having trouble with the save buttons callback function. Trying to pass parameters to OLEdb command without much luck.

---
+++ ISA_account_manager/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace ISA_account_manager
+{
+    partial class outlaw_hess_frm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.customersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.new_customer_btn = new System.Windows.Forms.ToolStripMenuItem();
+            this.view_customers_btn = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.new_product_btn = new System.Windows.Forms.ToolStripMenuItem();
+            this.view_products_btn = new System.Windows.Forms.ToolStripMenuItem();
+            this.accountsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.new_account_btn = new System.Windows.Forms.ToolStripMenuItem();
+            this.view_accounts_btn = new System.Windows.Forms.ToolStripMenuItem();
+            this.transactionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.new_transaction_btn = new System.Windows.Forms.ToolStripMenuItem();
+            this.view_transactions_btn = new System.Windows.Forms.ToolStripMenuItem();
+            this.main_panel = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.customersToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.accountsToolStripMenuItem,
+            this.transactionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // customersToolStripMenuItem
+            // 
+            this.customersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.new_customer_btn,
+            this.view_customers_btn});
+            this.customersToolStripMenuItem.Name = "customersToolStripMenuItem";
+            this.customersToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.customersToolStripMenuItem.Text = "Customers";
+            // 
+            // new_customer_btn
+            // 
+            this.new_customer_btn.Name = "new_customer_btn";
+            this.new_customer_btn.Size = new System.Drawing.Size(157, 22);
+            this.new_customer_btn.Text = "New customer";
+            this.new_customer_btn.Click += new System.EventHandler(this.new_customer_btn_Click);
+            // 
+            // view_customers_btn
+            // 
+            this.view_customers_btn.Name = "view_customers_btn";
+            this.view_customers_btn.Size = new System.Drawing.Size(157, 22);
+            this.view_customers_btn.Text = "View customers";
+            this.view_customers_btn.Click += new System.EventHandler(this.view_customers_btn_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.new_product_btn,
+            this.view_products_btn});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(66, 20);
+            this.toolStripMenuItem1.Text = "Products";
+            // 
+            // new_product_btn
+            // 
+            this.new_product_btn.Name = "new_product_btn";
+            this.new_product_btn.Size = new System.Drawing.Size(149, 22);
+            this.new_product_btn.Text = "New product";
+            // 
+            // view_products_btn
+            // 
+            this.view_products_btn.Name = "view_products_btn";
+            this.view_products_btn.Size = new System.Drawing.Size(149, 22);
+            this.view_products_btn.Text = "View products";
+            // 
+            // accountsToolStripMenuItem
+            // 
+            this.accountsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.new_account_btn,
+            this.view_accounts_btn});
+            this.accountsToolStripMenuItem.Name = "accountsToolStripMenuItem";
+            this.accountsToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.accountsToolStripMenuItem.Text = "Accounts";
+            // 
+            // new_account_btn
+            // 
+            this.new_account_btn.Name = "new_account_btn";
+            this.new_account_btn.Size = new System.Drawing.Size(150, 22);
+            this.new_account_btn.Text = "New account";
+            // 
+            // view_accounts_btn
+            // 
+            this.view_accounts_btn.Name = "view_accounts_btn";
+            this.view_accounts_btn.Size = new System.Drawing.Size(150, 22);
+            this.view_accounts_btn.Text = "View accounts";
+            // 
+            // transactionsToolStripMenuItem
+            // 
+            this.transactionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.new_transaction_btn,
+            this.view_transactions_btn});
+            this.transactionsToolStripMenuItem.Name = "transactionsToolStripMenuItem";
+            this.transactionsToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.transactionsToolStripMenuItem.Text = "Transactions";
+            // 
+            // new_transaction_btn
+            // 
+            this.new_transaction_btn.Name = "new_transaction_btn";
+            this.new_transaction_btn.Size = new System.Drawing.Size(166, 22);
+            this.new_transaction_btn.Text = "New transaction";
+            // 
+            // view_transactions_btn
+            // 
+            this.view_transactions_btn.Name = "view_transactions_btn";
+            this.view_transactions_btn.Size = new System.Drawing.Size(166, 22);
+            this.view_transactions_btn.Text = "View transactions";
+            // 
+            // main_panel
+            // 
+            this.main_panel.Location = new System.Drawing.Point(12, 27);
+            this.main_panel.Name = "main_panel";
+            this.main_panel.Size = new System.Drawing.Size(776, 411);
+            this.main_panel.TabIndex = 1;
+            // 
+            // outlaw_hess_frm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.main_panel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "outlaw_hess_frm";
+            this.Text = "Outlaw Hess";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem customersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem new_customer_btn;
+        private System.Windows.Forms.ToolStripMenuItem view_customers_btn;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem new_product_btn;
+        private System.Windows.Forms.ToolStripMenuItem view_products_btn;
+        private System.Windows.Forms.ToolStripMenuItem accountsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem new_account_btn;
+        private System.Windows.Forms.ToolStripMenuItem view_accounts_btn;
+        private System.Windows.Forms.ToolStripMenuItem transactionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem new_transaction_btn;
+        private System.Windows.Forms.ToolStripMenuItem view_transactions_btn;
+        private System.Windows.Forms.Panel main_panel;
+    }
+}
+
---
+++ ISA_account_manager/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace ISA_account_manager
+{
+    public partial class outlaw_hess_frm : Form
+    {
+        ISA_db db = new ISA_db();
+
+        public outlaw_hess_frm()
+        {
+            InitializeComponent();
+            db.Connect();
+        }
+
+        public class ISA_db
+        {
+
+            //public OleDbCommand myCmd = new OleDbCommand();
+            public OleDbConnection myConn = new OleDbConnection(dbconnect);
+
+            public static string dbconnect = @"Provider=Microsoft.ACE.OLEDB.12.0;"
+            + "Data Source=" + Application.StartupPath + @"\database.accdb;";
+
+            public int Connect()
+            {
+                try
+                {
+                    myConn.Open();
+                }
+                catch (Exception e)
+                {
+                    myConn.Close();
+                    MessageBox.Show(e.Message.ToString());
+                }
+                return 0;
+            }
+
+            public int Execute(String command)
+            {
+                OleDbCommand cmd = new OleDbCommand(command, myConn);
+                cmd.ExecuteNonQuery();
+                return 0;
+            }
+        }
+
+        /* Creates an input form with the specificed number of inputs. */
+        private void generate_input_form(object[,] input, string command_text)
+        {
+            int start_x = 10;
+            int start_y = 10;
+
+            Label title = new Label
+            {
+                Text = "New Customer",
+                Location = new System.Drawing.Point(start_x, start_y),
+                Font = new Font("Arial", 20, FontStyle.Bold),
+                AutoSize = true
+            };
+            main_panel.Controls.Add(title);
+            title.BringToFront();
+            start_y = 50;
+
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                if (i % 4 == 0 && i != 0)
+                {
+                    start_x += 150;
+                    start_y = 50;
+                }
+                Label lbl = new Label
+                {
+                    Text = (string)input[i,0],
+                    Location = new System.Drawing.Point(start_x, start_y)
+                };
+                main_panel.Controls.Add(lbl);
+                start_y += 20;
+                /* Type casting. */
+                if (input[i, 1] is TextBox)
+                {
+                    TextBox element = (TextBox)input[i, 1];
+                    element.Location = new System.Drawing.Point(start_x, start_y);
+                    element.Width = 130;
+                    main_panel.Controls.Add(element);
+                    element.BringToFront();
+                }
+                if (input[i, 1] is DateTimePicker)
+                {
+                    DateTimePicker element = (DateTimePicker)input[i, 1];
+                    element.Location = new System.Drawing.Point(start_x, start_y);
+                    element.Width = 130;
+                    main_panel.Controls.Add(element);
+                    element.BringToFront();
+                }
+                start_y += 25;
+            }
+            /* Save button. */
+            Button save_btn = new Button()
+            {
+                Width = 130,
+                Location = new System.Drawing.Point(10, 60 + (45 * 4)),
+                Text = "Save"
+            };
+            main_panel.Controls.Add(save_btn);
+            save_btn.BringToFront();
+            /* Event listener. */
+            save_btn.Click += (s, e) => {
+
+
+                OleDbCommand command = new OleDbCommand();
+
+                // Set the Connection, CommandText and Parameters.
+                //command.Connection = db.myConn;
+                command.CommandText = command_text;
+                //command.Parameters.Add("title", OleDbType.Integer, 6);
+                //command.Parameters[0].Value = 20;
+
+                string[] columns =
+                {
+                    "title", "firstname", "lastname", "dob", "natins", "email", "pswd"
+
+                };
+
+                for (int i = 0; i < input.GetLength(0); i++)
+                {
+     
+                    if (input[i, 1] is TextBox)
+                    {
+                        TextBox element = (TextBox)input[i, 1];
+                        command.Parameters.Add(columns[i], OleDbType.VarChar, 50);
+                        command.Parameters[i].Value = element.Text;
+                    }
+                    if (input[i, 1] is DateTimePicker)
+                    {
+                        DateTimePicker element = (DateTimePicker)input[i, 1];
+                        command.Parameters.Add(columns[i], OleDbType.Date);
+                        command.Parameters[i].Value = element.Text;
+                    }
+   
+                }
+                MessageBox.Show(command.Parameters[0].ToString());
+                //command.Prepare();
+                
+                
+                // cmd.ExecuteNonQuery();
+
+                /* Execute Query. */
+                // db.Execute(command);
+                /* Send feedback. */
+            };
+        }
+
+        private void new_customer_btn_Click(object sender, EventArgs e)
+        {
+
+            object[,] input = new object[7,2]
+            {
+                {"Title", new TextBox(){ Text = "" } },
+                {"First Name", new TextBox(){ Text = "" } },
+                {"Last Name", new TextBox(){ Text = "" } },
+                {"Date of Birth", new DateTimePicker() },
+                {"National Insurance", new TextBox(){ Text = "" } },
+                {"Email", new TextBox(){ Text = "" } },
+                {"Password", new TextBox(){ Text = "" } }
+            };
+            //TextBox test = (TextBox)input[0, 1];
+            main_panel.Controls.Clear();
+            /* Generate Command */
+            String command_text = "INSERT INTO dbo.customers (title, firstname, lastname, dob, natins, email, pswd) VALUES ( ?, ?, ?, ?, ?, ?, ? )";
+            /* Generate form. */
+            generate_input_form(input, command_text);
+
+
+            /* 
+            // Create the Command.
+            OleDbCommand command = new OleDbCommand();
+
+            // Set the Connection, CommandText and Parameters.
+            command.CommandText =
+                "INSERT INTO dbo.Region (RegionID, RegionDescription) VALUES (?, ?)";
+            command.Parameters.Add("RegionID", OleDbType.Integer, 4);
+            command.Parameters.Add("RegionDescription", OleDbType.VarWChar, 50);
+            command.Parameters[0].Value = 20;
+            command.Parameters[1].Value = "First Region";
+
+            MessageBox.Show(command.Parameters[1].Value.ToString());
+            */
+        }
+
+
+
+
+        private void view_customers_btn_Click(object sender, EventArgs e)
+        {
+            main_panel.Controls.Clear();
+            DataGridView data_grid = new DataGridView()
+            {
+
+            };
+            main_panel.Controls.Add(data_grid);
+        }
+    }
+}
