91bc4ec6ba319c1aeceec09966e64c44a03ac51a
Max Rochefort-Shugar <max.shugar@sony.com>
2019-03-19 20:10:51 +0000

    Can now add accounts, transactions and view all four tables.

--- ISA_account_manager/Form1.Designer.cs
+++ ISA_account_manager/Form1.Designer.cs
@@ -47,6 +47,7 @@
             // 
             // menuStrip1
             // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.customersToolStripMenuItem,
             this.toolStripMenuItem1,
@@ -54,7 +55,8 @@
             this.transactionsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(1200, 35);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -64,20 +66,20 @@
             this.new_customer_btn,
             this.view_customers_btn});
             this.customersToolStripMenuItem.Name = "customersToolStripMenuItem";
-            this.customersToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.customersToolStripMenuItem.Size = new System.Drawing.Size(109, 29);
             this.customersToolStripMenuItem.Text = "Customers";
             // 
             // new_customer_btn
             // 
             this.new_customer_btn.Name = "new_customer_btn";
-            this.new_customer_btn.Size = new System.Drawing.Size(157, 22);
+            this.new_customer_btn.Size = new System.Drawing.Size(220, 30);
             this.new_customer_btn.Text = "New customer";
             this.new_customer_btn.Click += new System.EventHandler(this.new_customer_btn_Click);
             // 
             // view_customers_btn
             // 
             this.view_customers_btn.Name = "view_customers_btn";
-            this.view_customers_btn.Size = new System.Drawing.Size(157, 22);
+            this.view_customers_btn.Size = new System.Drawing.Size(220, 30);
             this.view_customers_btn.Text = "View customers";
             this.view_customers_btn.Click += new System.EventHandler(this.view_customers_btn_Click);
             // 
@@ -87,20 +89,22 @@
             this.new_product_btn,
             this.view_products_btn});
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(66, 20);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(94, 29);
             this.toolStripMenuItem1.Text = "Products";
             // 
             // new_product_btn
             // 
             this.new_product_btn.Name = "new_product_btn";
-            this.new_product_btn.Size = new System.Drawing.Size(149, 22);
+            this.new_product_btn.Size = new System.Drawing.Size(209, 30);
             this.new_product_btn.Text = "New product";
+            this.new_product_btn.Click += new System.EventHandler(this.new_product_btn_Click);
             // 
             // view_products_btn
             // 
             this.view_products_btn.Name = "view_products_btn";
-            this.view_products_btn.Size = new System.Drawing.Size(149, 22);
+            this.view_products_btn.Size = new System.Drawing.Size(252, 30);
             this.view_products_btn.Text = "View products";
+            this.view_products_btn.Click += new System.EventHandler(this.view_products_btn_Click);
             // 
             // accountsToolStripMenuItem
             // 
@@ -108,20 +112,22 @@
             this.new_account_btn,
             this.view_accounts_btn});
             this.accountsToolStripMenuItem.Name = "accountsToolStripMenuItem";
-            this.accountsToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.accountsToolStripMenuItem.Size = new System.Drawing.Size(97, 29);
             this.accountsToolStripMenuItem.Text = "Accounts";
             // 
             // new_account_btn
             // 
             this.new_account_btn.Name = "new_account_btn";
-            this.new_account_btn.Size = new System.Drawing.Size(150, 22);
+            this.new_account_btn.Size = new System.Drawing.Size(208, 30);
             this.new_account_btn.Text = "New account";
+            this.new_account_btn.Click += new System.EventHandler(this.new_account_btn_Click);
             // 
             // view_accounts_btn
             // 
             this.view_accounts_btn.Name = "view_accounts_btn";
-            this.view_accounts_btn.Size = new System.Drawing.Size(150, 22);
+            this.view_accounts_btn.Size = new System.Drawing.Size(252, 30);
             this.view_accounts_btn.Text = "View accounts";
+            this.view_accounts_btn.Click += new System.EventHandler(this.view_accounts_btn_Click);
             // 
             // transactionsToolStripMenuItem
             // 
@@ -129,36 +135,40 @@
             this.new_transaction_btn,
             this.view_transactions_btn});
             this.transactionsToolStripMenuItem.Name = "transactionsToolStripMenuItem";
-            this.transactionsToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.transactionsToolStripMenuItem.Size = new System.Drawing.Size(120, 29);
             this.transactionsToolStripMenuItem.Text = "Transactions";
             // 
             // new_transaction_btn
             // 
             this.new_transaction_btn.Name = "new_transaction_btn";
-            this.new_transaction_btn.Size = new System.Drawing.Size(166, 22);
+            this.new_transaction_btn.Size = new System.Drawing.Size(252, 30);
             this.new_transaction_btn.Text = "New transaction";
+            this.new_transaction_btn.Click += new System.EventHandler(this.new_transaction_btn_Click);
             // 
             // view_transactions_btn
             // 
             this.view_transactions_btn.Name = "view_transactions_btn";
-            this.view_transactions_btn.Size = new System.Drawing.Size(166, 22);
+            this.view_transactions_btn.Size = new System.Drawing.Size(252, 30);
             this.view_transactions_btn.Text = "View transactions";
+            this.view_transactions_btn.Click += new System.EventHandler(this.view_transactions_btn_Click);
             // 
             // main_panel
             // 
-            this.main_panel.Location = new System.Drawing.Point(12, 27);
+            this.main_panel.Location = new System.Drawing.Point(18, 42);
+            this.main_panel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.main_panel.Name = "main_panel";
-            this.main_panel.Size = new System.Drawing.Size(776, 411);
+            this.main_panel.Size = new System.Drawing.Size(1164, 632);
             this.main_panel.TabIndex = 1;
             // 
             // outlaw_hess_frm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.main_panel);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "outlaw_hess_frm";
             this.Text = "Outlaw Hess";
             this.menuStrip1.ResumeLayout(false);
--- ISA_account_manager/Form1.cs
+++ ISA_account_manager/Form1.cs
@@ -47,18 +47,19 @@ namespace ISA_account_manager
         }
 
         /* Creates an input form with the specificed number of inputs. */
-        private void generate_input_form(object[,] input, string command_text)
+        private void generate_input_form(string title_text, object[,] input, string command_text)
         {
             int start_x = 10;
             int start_y = 10;
 
             Label title = new Label
             {
-                Text = "New Customer",
+                Text = title_text,
                 Location = new System.Drawing.Point(start_x, start_y),
                 Font = new Font("Arial", 20, FontStyle.Bold),
                 AutoSize = true
             };
+
             main_panel.Controls.Add(title);
             title.BringToFront();
             start_y = 50;
@@ -94,14 +95,31 @@ namespace ISA_account_manager
                     main_panel.Controls.Add(element);
                     element.BringToFront();
                 }
+                if (input[i, 1] is CheckBox)
+                {
+                    CheckBox element = (CheckBox)input[i, 1];
+                    element.Location = new System.Drawing.Point(start_x, start_y);
+                    element.Width = 130;
+                    main_panel.Controls.Add(element);
+                    element.BringToFront();
+                }
+                if (input[i, 1] is ComboBox)
+                {
+                    ComboBox element = (ComboBox)input[i, 1];
+                    element.Location = new System.Drawing.Point(start_x, start_y);
+                    element.Width = 250;
+                    main_panel.Controls.Add(element);
+                    element.BringToFront();
+                }
                 start_y += 25;
             }
+            start_y += 15;
             /* Save button. */
             Button save_btn = new Button()
             {
                 Width = 130,
-                Location = new System.Drawing.Point(10, 60 + (45 * 4)),
-                Text = "Save"
+                Location = new System.Drawing.Point(start_x, start_y),
+            Text = "Save"
             };
             main_panel.Controls.Add(save_btn);
             save_btn.BringToFront();
@@ -136,7 +154,25 @@ namespace ISA_account_manager
                             is_empty = true;
                             break;
                         }
+
                     }
+                    if (input[i, 1] is CheckBox)
+                    {
+                        CheckBox element = (CheckBox)input[i, 1];
+                        command.Parameters.Add("?", OleDbType.Boolean).Value = element.Checked;
+                        
+                    }
+
+                    if (input[i, 1] is ComboBox)
+                    {
+                        /* Get id. */
+                        ComboBox element = (ComboBox)input[i, 1];
+                        String id = element.Text.Split('-')[0].Split(' ')[0];
+                        command.Parameters.Add("?", OleDbType.VarChar, 50).Value = id;
+
+                    }
+
+
                 }
 
                 if (is_empty)
@@ -167,25 +203,333 @@ namespace ISA_account_manager
                 {"Email", new TextBox(){ Text = "" } },
                 {"Password", new TextBox(){ Text = "" } }
             };
-            //TextBox test = (TextBox)input[0, 1];
             main_panel.Controls.Clear();
             /* Generate Command */
             String command_text = "INSERT INTO customers (title, firstname, lastname, dob, natins, email, pswd) VALUES ( ?, ?, ?, ?, ?, ?, ? )";
            
             /* Generate form. */
-            generate_input_form(input, command_text);
+            generate_input_form("Customers", input, command_text);
 
         }
 
-
-
-
         private void view_customers_btn_Click(object sender, EventArgs e)
         {
+
             main_panel.Controls.Clear();
+
+            int start_x = 10;
+            int start_y = 10;
+
+            Label title = new Label
+            {
+                Text = "Customers",
+                Location = new System.Drawing.Point(start_x, start_y),
+                Font = new Font("Arial", 20, FontStyle.Bold),
+                AutoSize = true
+            };
+
+            main_panel.Controls.Add(title);
+            start_y += 40;
+
+            /* Get customers. */
+            System.Data.DataSet ds = new System.Data.DataSet();
+            OleDbCommand command = new OleDbCommand()
+            {
+                CommandText = "SELECT * FROM customers",
+                Connection = db.myConn
+            };
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(command);
+            adapter.Fill(ds, "customers");
+
+            
             DataGridView data_grid = new DataGridView()
             {
+                DataSource = ds.Tables["customers"],
+                Width = 600,
+                Height = 350,
+                Location = new System.Drawing.Point(start_x, start_y),
+            };
+            main_panel.Controls.Add(data_grid);
+        }
 
+        /* Prevents characters being entered into a text box. */
+        private void no_char_func(object s, KeyPressEventArgs e)
+        {
+            /* Determine if dot has been used. */
+            TextBox local = (TextBox)s;
+            if(local.Text.IndexOf('.') != -1)
+            {
+                if(e.KeyChar == '.')
+                    e.Handled = true;
+            }
+
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != '.')
+                e.Handled = true;
+        }
+
+        private void new_product_btn_Click(object sender, EventArgs e)
+        {
+            /* Prevent characters being typed in interest rate text box. */
+            TextBox no_char = new TextBox();
+            no_char.KeyPress += new KeyPressEventHandler(no_char_func);
+            
+            object[,] input = new object[4, 2]
+            {
+                {"name", new TextBox(){ Text = "" } },
+                {"status", new TextBox(){ Text = "" } },
+                {"transactions in", new CheckBox() },
+                {"interest rate", no_char }
+            };
+
+            main_panel.Controls.Clear();
+            /* Generate Command */
+            String command_text = "INSERT INTO products (prod_name, status, transin, inrate) VALUES (?, ?, ?, ?)";
+
+            /* Generate form. */
+            generate_input_form("Products", input, command_text);
+        }
+
+        private void new_account_btn_Click(object sender, EventArgs e)
+        {
+            /* Prevent characters being typed in interest rate text box. */
+            TextBox no_char = new TextBox();
+            no_char.KeyPress += new KeyPressEventHandler(no_char_func);
+
+            /* Get customers. */
+            System.Data.DataSet ds = new System.Data.DataSet();
+            OleDbCommand command = new OleDbCommand()
+            {
+                CommandText = "SELECT custid, firstname, lastname, email FROM customers",
+                Connection = db.myConn
+            };
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(command);
+            adapter.Fill(ds, "customers");
+
+            ComboBox customer_cb = new ComboBox()
+            {
+                DropDownStyle = ComboBoxStyle.DropDown,
+                AutoCompleteMode = AutoCompleteMode.SuggestAppend,
+                AutoCompleteSource = AutoCompleteSource.ListItems
+            };
+
+            foreach (System.Data.DataRow dr in ds.Tables["customers"].Rows)
+                customer_cb.Items.Add(dr["custid"] + " - " + dr["firstname"] + " " + dr["lastname"] + " - " + dr["email"]);
+
+            /* Get products. */
+            command = new OleDbCommand()
+            {
+                CommandText = "SELECT prodid, prod_name, inrate FROM products",
+                Connection = db.myConn
+            };
+
+            adapter = new OleDbDataAdapter(command);
+            adapter.Fill(ds, "products");
+
+            ComboBox products_cb = new ComboBox()
+            {
+                DropDownStyle = ComboBoxStyle.DropDown,
+                AutoCompleteMode = AutoCompleteMode.SuggestAppend,
+                AutoCompleteSource = AutoCompleteSource.ListItems
+            };
+
+            foreach (System.Data.DataRow dr in ds.Tables["products"].Rows)
+                products_cb.Items.Add(dr["prodid"] + " - " + dr["prod_name"] + " - " + dr["inrate"] + "%");
+
+            /* Get products. */
+            object[,] input = new object[3, 2]
+            {
+                {"customer", customer_cb },
+                {"product", products_cb, },
+                {"balance £", no_char }
+            };
+
+            main_panel.Controls.Clear();
+            /* Generate Command */
+            DateTime thisDay = DateTime.Today;
+            String command_text = "INSERT INTO accounts (custid, prodid, balance, accrued, active, opnd) VALUES (?, ?, ?, '0', 'false', '" + thisDay.ToString("d") + "')";
+
+            /* Generate form. */
+            generate_input_form("Accounts", input, command_text);
+        }
+
+        private void new_transaction_btn_Click(object sender, EventArgs e)
+        {
+
+            /* Prevent characters being typed in amount text box and limit to one decimal point. */
+            TextBox no_char = new TextBox();
+            no_char.KeyPress += new KeyPressEventHandler(no_char_func);
+
+            /* Get customers. */
+            System.Data.DataSet ds = new System.Data.DataSet();
+            OleDbCommand command = new OleDbCommand()
+            {
+                CommandText = "SELECT accid, firstname, lastname, email FROM accounts INNER JOIN customers ON accounts.custid = customers.custid;",
+                Connection = db.myConn
+            };
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(command);
+            adapter.Fill(ds, "accounts");
+
+            ComboBox acc_cb = new ComboBox()
+            {
+                DropDownStyle = ComboBoxStyle.DropDown,
+                AutoCompleteMode = AutoCompleteMode.SuggestAppend,
+                AutoCompleteSource = AutoCompleteSource.ListItems
+            };
+
+            foreach (System.Data.DataRow dr in ds.Tables["accounts"].Rows)
+                acc_cb.Items.Add(dr["accid"] + " - " + dr["firstname"] + " " + dr["lastname"] + " - " + dr["email"]);
+
+            ComboBox actn_cb = new ComboBox()
+            {
+                DropDownStyle = ComboBoxStyle.DropDown,
+                AutoCompleteMode = AutoCompleteMode.SuggestAppend,
+                AutoCompleteSource = AutoCompleteSource.ListItems
+            };
+
+            actn_cb.Items.Add("IN");
+            actn_cb.Items.Add("OUT");
+            actn_cb.Items.Add("DEPOSIT");
+            actn_cb.Items.Add("WITHDRAWAL");
+
+            /* Get products. */
+            object[,] input = new object[3, 2]
+            {
+                {"account", acc_cb },
+                {"action", actn_cb, },
+                {"amount £", no_char }
+            };
+
+            main_panel.Controls.Clear();
+            /* Generate Command */
+            DateTime thisDay = DateTime.Today;
+            String command_text = "INSERT INTO tranx (accid, [action], amnt, [event]) VALUES (?, ?, ?, '" + thisDay.ToString("d") + "')";
+
+            /* Generate form. */
+            generate_input_form("Transaction", input, command_text);
+
+
+        }
+
+        private void view_products_btn_Click(object sender, EventArgs e)
+        {
+
+            main_panel.Controls.Clear();
+
+            int start_x = 10;
+            int start_y = 10;
+
+            Label title = new Label
+            {
+                Text = "Products",
+                Location = new System.Drawing.Point(start_x, start_y),
+                Font = new Font("Arial", 20, FontStyle.Bold),
+                AutoSize = true
+            };
+
+            main_panel.Controls.Add(title);
+            start_y += 40;
+
+            /* Get customers. */
+            System.Data.DataSet ds = new System.Data.DataSet();
+            OleDbCommand command = new OleDbCommand()
+            {
+                CommandText = "SELECT * FROM products",
+                Connection = db.myConn
+            };
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(command);
+            adapter.Fill(ds, "products");
+
+
+            DataGridView data_grid = new DataGridView()
+            {
+                DataSource = ds.Tables["products"],
+                Width = 600,
+                Height = 350,
+                Location = new System.Drawing.Point(start_x, start_y),
+            };
+            main_panel.Controls.Add(data_grid);
+        }
+
+        private void view_accounts_btn_Click(object sender, EventArgs e)
+        {
+            main_panel.Controls.Clear();
+
+            int start_x = 10;
+            int start_y = 10;
+
+            Label title = new Label
+            {
+                Text = "Accounts",
+                Location = new System.Drawing.Point(start_x, start_y),
+                Font = new Font("Arial", 20, FontStyle.Bold),
+                AutoSize = true
+            };
+
+            main_panel.Controls.Add(title);
+            start_y += 40;
+
+            /* Get customers. */
+            System.Data.DataSet ds = new System.Data.DataSet();
+            OleDbCommand command = new OleDbCommand()
+            {
+                CommandText = "SELECT * FROM accounts",
+                Connection = db.myConn
+            };
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(command);
+            adapter.Fill(ds, "accounts");
+
+
+            DataGridView data_grid = new DataGridView()
+            {
+                DataSource = ds.Tables["accounts"],
+                Width = 600,
+                Height = 350,
+                Location = new System.Drawing.Point(start_x, start_y),
+            };
+            main_panel.Controls.Add(data_grid);
+        }
+
+        private void view_transactions_btn_Click(object sender, EventArgs e)
+        {
+            main_panel.Controls.Clear();
+
+            int start_x = 10;
+            int start_y = 10;
+
+            Label title = new Label
+            {
+                Text = "Transactions",
+                Location = new System.Drawing.Point(start_x, start_y),
+                Font = new Font("Arial", 20, FontStyle.Bold),
+                AutoSize = true
+            };
+
+            main_panel.Controls.Add(title);
+            start_y += 40;
+
+            /* Get customers. */
+            System.Data.DataSet ds = new System.Data.DataSet();
+            OleDbCommand command = new OleDbCommand()
+            {
+                CommandText = "SELECT * FROM tranx",
+                Connection = db.myConn
+            };
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(command);
+            adapter.Fill(ds, "tranx");
+
+
+            DataGridView data_grid = new DataGridView()
+            {
+                DataSource = ds.Tables["tranx"],
+                Width = 600,
+                Height = 350,
+                Location = new System.Drawing.Point(start_x, start_y),
             };
             main_panel.Controls.Add(data_grid);
         }
